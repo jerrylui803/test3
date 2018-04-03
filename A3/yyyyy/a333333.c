c3c3c1c5198e1201809b2fe017b054b9ce93ece3
Yan lu <yan@Yans-MacBook-Pro.local>
2018-04-03 18:50:56 -0400

    fsndhjfbads

--- A3/yyyyy/a333333.c
+++ A3/yyyyy/a333333.c
@@ -6,28 +6,25 @@
 #include <sys/mman.h>
 
 int main(int argc, char *argv[]) {
-
+	char *path;
 	// name of disk. (Option flag -a). path. 
 	if (argc <= 2 || argc >= 5){
 		fprintf(stderr, "Wrong argruments.\n");
 		exit(1);
 	// if without the flag
 	} else if (argc == 3) {
-		if (argv[2][0] != '/'){
-			fprintf(stderr, "Not an absolute path.\n");
-			exit(1);
-		}
+		path = argv[2];
 	// if there is a flag
 	} else if (argc == 4) { 
-		if (argv[3][0] != '/'){
-			fprintf(stderr, "Not an absolute path.\n");
-			exit(1);
-		}
-
+		path = argv[3];
 		if (strcmp(argv[2], "-a") != 0) {
 			fprintf(stderr, "Not a vaild flag.\n");
 			exit(1);
 		}
+	}
+	if (path[0] != '/'){
+		fprintf(stderr, "Not an absolute path.\n");
+		exit(1);
 	}
  
 	int fd = open(argv[1], O_RDWR);
@@ -38,6 +35,28 @@ int main(int argc, char *argv[]) {
 		perror("mmap");
 		exit(1);
 	}
+
+	//Get each file name from path
+	char *p = strtok(path, "/");
+	// hard code.
+	char *dirArray[10];
+	int i = 0;
+	dirArray[0] = p;
+	while (p != NULL){
+		dirArray[i++] = p;
+		p = strtok (NULL, "/");
+
+	}
+
+	for (int j=0; j < sizeof(dirArray); j++){
+		printf("%s \n", dirArray[j]);
+	}
+
+	//Compare inodes with each tokenarray name?
+	
+
+
+
 	// TUT
 	struct ext2_super_block *sb = (struct ext2_super_block *) (disk + 1024);
 	printf("Inodes: %d\n", sb->s_inodes_count);
