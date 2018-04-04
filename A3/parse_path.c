92fb50f028d6ab7924a7c35f6e769c973b5c0483
Jerry Lui <jerrylui803@gmail.com>
2018-04-04 15:10:59 -0400

    finish parser

--- A3/parse_path.c
+++ A3/parse_path.c
@@ -4,15 +4,56 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <stdlib.h>
+
+#include <string.h>
 
 
+char **parse_file_path(char *path, int * path_size){
+	int i = 0;
+    int n = 0;
+    // find path size
+    while (path[i] != '\0') {
+        if (path[i] == '/')
+            n++;
+        i++;
+    }
+    // if the path name ends with /
+    if(path[i - 1] == '/') 
+        n--;
 
+    char **path_array = malloc(n * (sizeof (char *)));
+	//Get each file name from path
+	char *p = strtok(path, "/");
+	i = 0;
+	while (p != NULL){
+		path_array[i] = malloc(EXT2_NAME_LEN);
+		strcpy(path_array[i], p);
+		i++;
+		p = strtok (NULL, "/");
+	}
+
+	*path_size = n;
+	return path_array;
+}
 
 
 
 int main(int argc, char *argv[]) {
 
+	char ** path_array;
+	int path_size;
 
+	path_array = parse_file_path(argv[1], &path_size);
+
+	printf("%d \n", path_size);
+
+	for (int i = 0; i < path_size; i++)
+		printf("%s \n", path_array[i]);
+
+
+
+	/*
 	char *path = argv[1];
 	//Get each file name from path
 	char *p = strtok(path, "/");
@@ -31,6 +72,7 @@ int main(int argc, char *argv[]) {
 	for (int j=0; j < numFiles; j++){
 		printf("%s \n", dirArray[j]);
 	}
+	*/
 
 
 
